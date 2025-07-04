b269d77a9934e68ac76147b95c331dbfafbd108b
Xeonilian <zhengws@ier.org.cn>
2025-07-04 09:02:43 +0800

    adj: [timetable] 合并相关type

--- src/core/types/Block.ts
+++ src/core/types/Block.ts
@@ -6,3 +6,35 @@ export interface Block {
   start: string; //使用HH:MM
   end: string;
 }
+
+export interface PomodoroSegment {
+  parentBlockId: string;
+  type: "work" | "break" | "schedule" | "untaetigkeit";
+  start: number;
+  end: number;
+  category: string; // 原block的类型
+  index?: number; // 在同种类型中的序号
+}
+
+export interface TodoSegment {
+  todoId: number;
+  todoTitle: string;
+  priority: number;
+  start: number;
+  end: number;
+  pomoType: "🍅" | "🍇" | "🍒";
+  category?: string;
+  index: number; // 本todo第几个番茄
+  assignedPomodoroSegment?: PomodoroSegment;
+  overflow?: boolean; // 是否溢出（超出可用时间段）
+  completed?: boolean; // todo是否已完成
+  usingRealPomo?: boolean; // 是否使用realPomo计数
+}
+
+export interface ActualTimeRange {
+  todoId: number;
+  todoTitle: string;
+  start: number;
+  end: number;
+  category: string;
+}
