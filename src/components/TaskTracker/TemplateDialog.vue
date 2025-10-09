e022558e40a328fbe65c683286e15ad6ce825070
Xeonilian <zhengws@ier.org.cn>
2025-10-09 10:00:34 +0800

    ui(task): 重构书写模版

--- src/components/TaskTracker/TemplateDialog.vue
+++ src/components/TaskTracker/TemplateDialog.vue
@@ -1,119 +1,74 @@
 <template>
-  <n-modal
-    v-model:show="showModal"
-    preset="dialog"
-    title="书写模板"
-    :on-after-leave="handleCancel"
-    style="width: 500px; height: 400px"
-  >
-    <n-space vertical size="large">
-      <n-layout has-sider>
-        <n-layout-sider
-          content-style="padding: 0; overflow-y: auto; max-height: 300px; background-color:var(--color-background-light)"
-          width="150"
-        >
-          <!-- 左侧：模板列表 -->
-          <n-list bordered>
-            <n-list-item
-              v-for="template in templates"
-              :key="template.id"
-              @click="selectTemplate(template)"
-              @dblclick="copyTemplateContent(template)"
-              title="双击复制模板"
-              :style="{
-                background:
-                  selectedTemplate?.id === template.id
-                    ? 'var(--color-blue-light)'
-                    : 'var(--color-background',
-              }"
-            >
-              {{ template.title }}
-            </n-list-item>
-          </n-list>
-        </n-layout-sider>
+  <n-modal v-model:show="showModal" preset="dialog" title="书写模板" :on-after-leave="handleCancel" style="width: 500px; height: 410px">
+    <n-layout has-sider class="template-container">
+      <n-layout-sider bordered width="150">
+        <!-- 左侧：模板列表 -->
+        <n-list>
+          <n-list-item
+            v-for="template in templates"
+            :key="template.id"
+            @click="selectTemplate(template)"
+            @dblclick="copyTemplateContent(template)"
+            title="双击复制模板"
+            :style="{
+              background: selectedTemplate?.id === template.id ? 'var(--color-blue-light)' : 'var(--color-background)',
+            }"
+          >
+            {{ template.title }}
+          </n-list-item>
+        </n-list>
+      </n-layout-sider>
 
-        <!-- 右侧：模板内容编辑区域 -->
-        <n-layout-content
-          content-style="padding:0 2px; display: flex; flex-direction: column; height: 100%;"
-        >
-          <div style="flex: 1; overflow: hidden">
-            <n-input
-              type="text"
-              v-model:value="editableTemplateTitle"
-              :disabled="!canEditContent"
-              :placeholder="contentPlaceholder"
-              style="width: 100%"
-              :style="{
-                background: canEditContent
-                  ? 'var(--color-blue-light)'
-                  : 'var(--color-background)',
-              }"
-            />
-            <n-input
-              type="textarea"
-              v-model:value="editableTemplateContent"
-              :placeholder="contentPlaceholder"
-              rows="10"
-              :disabled="!canEditContent"
-              style="width: 100%"
-              :style="{
-                background: !canEditContent
-                  ? 'var(--color-background-light)'
-                  : 'var(--color-background)',
-              }"
-            />
-          </div>
-        </n-layout-content>
-      </n-layout>
+      <!-- 右侧：模板内容编辑区域 -->
+      <n-layout-content>
+        <n-input
+          type="text"
+          v-model:value="editableTemplateTitle"
+          :disabled="!canEditContent"
+          :placeholder="contentPlaceholder"
+          style="width: 100%; height: 30px"
+          :style="{
+            background: canEditContent ? 'var(--color-blue-light)' : 'var(--color-background)',
+          }"
+        />
+        <n-input
+          type="textarea"
+          v-model:value="editableTemplateContent"
+          :placeholder="contentPlaceholder"
+          rows="10"
+          :disabled="!canEditContent"
+          style="width: 100%; height: calc(100% - 31px)"
+          :style="{
+            background: !canEditContent ? 'var(--color-background-light)' : 'var(--color-background)',
+          }"
+        />
+      </n-layout-content>
+    </n-layout>
 
-      <n-layout-footer>
-        <n-space justify="center">
-          <n-button
-            type="info"
-            secondary
-            :disabled="!selectedTemplate"
-            @click="handleCopy"
-          >
-            {{ copyMessage }}
-          </n-button>
-          <n-button
-            @click="handleAddNewTemplate"
-            type="primary"
-            :title="'新增模板'"
-            >新增</n-button
-          >
-          <n-button
-            type="primary"
-            @click="handleConfirm"
-            :disabled="!selectedTemplate && !addNew"
-            :title="addNew ? '确认新增模版' : '确认编辑模版'"
-            >确认</n-button
-          >
-          <n-button
-            type="default"
-            :disabled="!selectedTemplate"
-            @click="handleDelete"
-          >
-            删除
-          </n-button>
-          <n-button @click="handleCancel">取消</n-button>
-        </n-space>
-      </n-layout-footer>
-    </n-space>
+    <n-layout-footer>
+      <n-space justify="center">
+        <n-button type="info" secondary :disabled="!selectedTemplate" @click="handleCopy">
+          {{ copyMessage }}
+        </n-button>
+        <n-button @click="handleAddNewTemplate" type="primary" :title="'新增模板'">新增</n-button>
+        <n-button
+          type="primary"
+          @click="handleConfirm"
+          :disabled="!selectedTemplate && !addNew"
+          :title="addNew ? '确认新增模版' : '确认编辑模版'"
+        >
+          确认
+        </n-button>
+        <n-button type="default" :disabled="!selectedTemplate" @click="handleDelete">删除</n-button>
+        <n-button @click="handleCancel">取消</n-button>
+      </n-space>
+    </n-layout-footer>
   </n-modal>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, watch } from "vue";
-import {
-  NModal,
-  NInput,
-  NButton,
-  NSpace,
-  NList,
-  NListItem,
-  NLayout,
-} from "naive-ui";
+import { NModal, NInput, NButton, NSpace, NList, NListItem, NLayout } from "naive-ui";
 import { Template } from "@/core/types/Template";
 
 const props = defineProps<{
@@ -241,26 +196,33 @@ const resetForm = () => {
 </script>
 <style scoped>
 /* 额外的样式调整 */
-.n-layout-sider {
-  max-height: calc(100% - 100px); /* 保证最大高度适应 */
-  overflow-y: auto; /* 使侧边栏支持滚动 */
+.template-container {
+  height: 305px;
+  width: 445px;
 }
+
+.n-layout-content {
+  background-color: var(--color-background-light);
+}
+
+.n-layout-sider {
+  width: 150px;
+}
+
 .n-list {
-  max-height: 300px; /* 根据需求调整最大高度 */
+  height: calc(100% - 2px);
+  background-color: var(--color-background-light);
+  border: 1px solid var(--color-background-light);
 }
 
 .n-list-item {
   padding: 4px 6px !important;
   min-height: 30px;
+  border-radius: 2px;
 }
 
 .n-layout-footer {
-  padding: 0px;
+  padding: 8px;
   background-color: var(--color-background);
-}
-
-.n-dialog {
-  padding: 0 0;
-  margin: 0;
 }
 </style>
