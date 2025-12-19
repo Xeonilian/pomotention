8a197f1472a78c805d907f23f1d4452dd1a8fa1f
xeonilian <zhengws@ier.org.cn>
2025-12-19 16:21:18 +0800

    feat(pwa): 初步设置

--- src/main.ts
+++ src/main.ts
@@ -36,3 +36,16 @@ app.use(pinia);
 
 app.use(router);
 app.mount("#app");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log("✅ Service Worker registered:", registration.scope);
+      },
+      (error) => {
+        console.error("❌ Service Worker registration failed:", error);
+      }
+    );
+  });
+}
