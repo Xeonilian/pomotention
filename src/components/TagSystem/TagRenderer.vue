1c37171e68c1b103ffbaa040834482144bcc61a5
Xeonilian <zhengws@ier.org.cn>
2026-01-14 23:19:25 +0800

    ui(planner): monthplanner tag dot border

--- src/components/TagSystem/TagRenderer.vue
+++ src/components/TagSystem/TagRenderer.vue
@@ -14,6 +14,7 @@
         color: tag.color,
         backgroundColor: tag.backgroundColor,
         // border: `1px solid ${tag.color}`,
+        border: 'none !important',
         boxShadow: props.displayLength === null || props.displayLength === 0 ? `3px -2px 0px 0px ${tag.color} inset` : 'none',
       }"
       displayLength="props.displayLength || null"
