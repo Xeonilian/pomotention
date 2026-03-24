a52d2a4a7cab3c29e921142444217e9e5bc16215
Xeonilian <zhengws@ier.org.cn>
2026-03-24 13:34:24 +0800

    feat(mobile): task和FBA互动

--- src/components/TaskTracker/TaskRecord.vue
+++ src/components/TaskTracker/TaskRecord.vue
@@ -104,6 +104,7 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: "update:content", content: string): void;
   (e: "activetaskId", taskId: number | null): void;
+  (e: "update:isEditing", value: boolean): void;
 }>();
 
 const content = ref(props.initialContent);
@@ -138,6 +139,14 @@ watch(
   (newContent) => {
     content.value = newContent;
   },
+);
+
+watch(
+  isEditing,
+  (v) => {
+    emit("update:isEditing", v);
+  },
+  { immediate: true },
 );
 
 const startEditing = () => {
